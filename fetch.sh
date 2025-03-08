e29c53abc8b617a7b5406e552ba45a50e7bf2d50
Andrew den Hertog <andrew@denhertog.ca>
2025-03-08 10:14:49 -0500

    feat: Update fetch script to fetch specific tag and create a timestamped branch

--- fetch.sh
+++ fetch.sh
@@ -6,8 +6,11 @@ git init -q
 git remote add origin https://github.com/VSCodium/vscodium.git
 git remote add dest https://github.com/andrewhertog/codex.git
 
-git fetch --depth 1 origin master
-git checkout master
-git checkout -b test
+git fetch origin
+git checkout 1.97.0.25037
+TAG=update/$(date +%Y-%m-%d-%H-%M-%S)
+git switch -c $TAG
 
 git pull dest master --allow-unrelated-histories -X theirs
+
+# gh pr create --repo andrewhertog/codex --base master
